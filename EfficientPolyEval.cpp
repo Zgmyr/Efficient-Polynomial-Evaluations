0dc9ac6b2bf061a2d253038c0898c1482c567658
Zgmyr <zacharygmyr@gmail.com>
2024-10-24 02:38:34 -0500

    Cleaned up test file, refined pseudocode for driver. Finished function for generating coefficients to external file using GNU multiple precision library.

--- EfficientPolyEval.cpp
+++ EfficientPolyEval.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <fstream>
-#include <random>
+#include <gmpxx.h>
 #include <math.h>
+#include <chrono>
+
+using namespace std::chrono;
 using namespace std;
 
+#define FILEPATH "coeffAndVars.txt"
+
 // prototype for generating coefficients & variables
-void generateCoeffAndVars(int,long int);
+void generateCoeffAndVars(int,int);
 
 int main() {
 
@@ -19,31 +24,40 @@ int main() {
         PASS N and D TO GENERATE COEFF&VAR TO UPDATE DATA
     (2) SKIP...
 
+    INITIALIZE FILE FOR READING FROM FILEPATH MACRO
     INITIALIZE VECTOR TO HOLD COEFFICIENTS & X
+    ITERATE THROUGH UNTIL EMTPY:
+        CONVERT THE LINE INTO mpz_class AND STORE IN VECTOR
+
+       *** => make sure to use &reference in range-based for loop later for reading from vector (this might get LARGE for large values n)
 
     MENU:
         1. BRUTE
         2. REPEATED
         3. HORNERS
-    
     (1 | 2 | 3) DO:
         PASS IN VECTOR OF COEFF TO ALGORITHM 
         START TIMER
         EVALUATES
         END TIMER
+
+       *** => will need to re-do the brute force algorithm for general case polynomial using coefficients read from vector
+    
     */
 
-   generateCoeffAndVars(15,10);
+   generateCoeffAndVars(35,25);
 
    return 0;
 }
 
-// function generates and writes to file (in order) (1) a value 'x' (range 1-100), and (2) 'n+1' list
-//      of 'd' digit sized coefficent values. This data is to be used by the three polynomial evaluation algorithms.
-/* ISSUES:
-    - can't go larger than 10 digits (overflow error of max int value)
-    - not uniformly 10 digit numbers (is this acceptable? Probably)*/
-void generateCoeffAndVars(int n, long int d) {
+/* This function uses GNU multi-precision to generate and write to an external file:
+    (1) a value 'x' (within the range 1 ... 100 for simplicty)
+    (2) 'n+1' list of 'd'-digit coefficient values (includes the constant term)
+   This data can then be read to evaluate polynomials P(x) = a_0 + a_1x^1 + a_2x^2 + ... + a_nx^n */
+void generateCoeffAndVars(int n, int d) {
+
+    // DEBUG - use this to see evaluation time of coefficient generation (see end of function)
+    /* time_point<steady_clock> start_time = steady_clock::now(); */
 
     // Validation: ensure n and d are at least 1
     if (n < 1 || d < 1)
@@ -53,33 +67,59 @@ void generateCoeffAndVars(int n, long int d) {
     }
 
     // initialize output file
-    ofstream inFile("coeffAndVars.txt");
+    ofstream inFile(FILEPATH);
 
     // ensure file opened properly
-    if (!inFile.is_open()) {
+    if (inFile.is_open()) {
+        cout << "> Generating " << n+1 << " new " << d << "-digit coefficient values in "
+            << "'coeffAndVars.txt' ..." << endl;
+    }
+    else {
         cout << "Error in opening file 'coeffAndVars.txt' for writing. Ending program..." << endl;
         exit(1);
     }
 
-    // seed randomizer for generating coefficients
-    srand(time(0));
-    random_device myEngine;
-    uniform_int_distribution<long int> randInt(1,2147483647);
+    // generate random value for 'x' & write to file
+    srand(time(0)); // random seed
+    int x = rand() % 100 + 1; // range is from 1 to 100, for simplicity
+    inFile << x << endl;
 
-    // generate random value for 'x' (for simplicity, between 1-100)
-    int x = rand() % 100 + 1;
-    inFile << x << endl; // write 'x' to file as first value
+    /* Initialize random state for large random number generation */
+    gmp_randstate_t rand_state;
+    gmp_randinit_mt(rand_state);  // Mersenne Twister algorithm for random state
+    gmp_randseed_ui(rand_state, time(0));  // Seed with the current time
+
+    // determine bit-count for 'd'-digits
+    double log2_10 = log(10.0) / log(2.0);
+    double bit_count = d * log2_10;
 
     // generate 'n+1' coefficients for the polynomial, each with 'd' digits
     for (int i = 0; i <= n; i++)
     {
-        // generate random 'd'-digit coefficient
-        int coefficient = randInt(myEngine) % static_cast<int>(pow(10,d));
+        /* GMP initializations */
+        mpz_t random_num; // for holding random coefficient
+        mpz_init(random_num);
 
-        inFile << coefficient << endl;
+        // generate random number of 'd'-digits using bitcount of 'd'
+        mpz_rrandomb(random_num, rand_state, bit_count);
+
+        inFile << random_num << endl;
+
+        mpz_clear(random_num); // cleanup
     }
 
-    cout << "DEBUG: Subprogram ran successfully" << endl;
-
+    // cleanup GMP & close file
+    gmp_randclear(rand_state);
     inFile.close();
+
+    // report successful operation
+    cout << "> 'coeffAndVars.txt' has successfully been updated with new coefficient values!" << endl;
+
+    // DEBUG - use this to see evaluation time of coefficient generation
+    /* duration<double> eval_time = steady_clock::now() - start_time;
+
+    double eval_s = eval_time.count();
+    double eval_ms = eval_s * 1000;
+
+    cout << "Process time (ms): " << eval_ms << endl; */
 }
--- test.cpp
+++ test.cpp
@@ -1,39 +1,70 @@
 // messing around with <gmpxx.h>
 
 #include <iostream>
-#include <fstream>
-#include <random>
-#include <chrono>
-#include <thread>
-#include <math.h>
+// #include <fstream>
+// #include <chrono>
+// #include <thread>
+// #include <math.h>
 #include <gmpxx.h>
+#include <vector>
 using namespace std;
-using namespace std::chrono;
+// using namespace std::chrono;
 
 int main() {
 
+
+
+    // vector<mpz_class> vector1;
+
+    // mpz_class val1 = 123456789;
+    // mpz_class val2 = 987654321;
+
+    // vector1.push_back(val1);
+    // vector1.push_back(val2);
+
+    // for (const auto& v : vector1) {
+    //     cout << v << endl;
+    // }
+
+    int arr1[100];
+    for (int i=0;i<100;i++) {
+        arr1[i] = i;
+    }
+    for (int& e : arr1) {
+        e *= 5;
+    }
+    for (const int& e : arr1) {
+        cout << e << endl;
+    }
+
+
+
     /* Chrono Test */
+    /* => use this to measure efficiency of algorithms (duration) */
 
-    // beginning reference point
-    // time_point<steady_clock> start = steady_clock::now();
+    /* beginning reference point
+    time_point<steady_clock> start = steady_clock::now();
 
-    // this_thread::sleep_for(5s);
+    this_thread::sleep_for(5s);
 
-    // // ending reference point
-    // time_point<steady_clock> end = steady_clock::now();
+    // ending reference point
+    time_point<steady_clock> end = steady_clock::now();
 
-    // // NOTE: duration<double> defaults to (s)
-    // // duration<double> elapsed_time = end - start;
-    // // OR
-    // duration<double> elapsed_time = steady_clock::now() - start;
+    // NOTE: duration<double> defaults to (s)
+    // duration<double> elapsed_time = end - start;
+    // OR
+    duration<double> elapsed_time = steady_clock::now() - start;
 
-    // cout << "Elapsed time (ms): " << duration_cast<milliseconds>(elapsed_time).count() << endl;
-
-
-
+    cout << "Elapsed time (ms): " << duration_cast<milliseconds>(elapsed_time).count() << endl; */
 
 
-    mpz_t random_num;
+
+
+
+
+    /* VERY LARGE RAND # GENERATION */
+
+    /* mpz_t random_num;
     gmp_randstate_t rand_state;
     
     // Initialize random state
@@ -49,30 +80,14 @@ int main() {
     double log2_10 = log(10.0) / log(2.0);
     double bit_count = d * log2_10;
     mpz_rrandomb(random_num, rand_state, bit_count);
-    cout << random_num << endl;
+    cout << random_num << endl; */
 
-    /*
-    PSEUDOCODE FOR GENERATING RANDOM 'd'-DIGIT #'s:
+
     
-    given values 'n' and 'd'
-    initialize vars (random state, any mpz_t values, etc.)
-        -> double log2_10 = log(10.0) / log(2.0);
-    take 'd' and convert to bits such that:
-        -> double bitcount = d * log2(10)
-    loop for n+1 times to generate:
-
-
-    */
-    
-
-
-
-
-
-
 
 
     /* RAND TEST*/
+    /* => SIMPLE. use for generating x */
 
     // cout << time(0) << endl;
 
@@ -94,27 +109,8 @@ int main() {
 
 
 
-    /* RAND TEST */
-    // random_device myEngine;
-    // uniform_int_distribution<int> randInt(0,100);
-
-    // int x = randInt(myEngine);
-    // cout << "(1) RANDOM INT = " << x << endl;
-
-    // x = randInt(myEngine);
-    // cout << "(2) AGAIN = " << x << endl;
-    
-    // cout << "THREE MORE TIMES:" << endl;
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     x = randInt(myEngine);
-    //     cout << x << endl;
-    // }
-
-
-
-
     /* FILE OUTPUT TEST */
+    /* testing file stream operations */
     // ofstream oFile("testdoc.txt", ios::app);
     
     // if (oFile.is_open()) {
